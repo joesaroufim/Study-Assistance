ed93c2174748f56bbca1a34d86e250f87fe74a44
Joe Saroufim <joesaroufim.os@gmail.com>
2022-05-04 14:04:49 +0300

    Set up the Cursor Object

--- app/src/main/java/com/lau/studyassistance/MainActivity.java
+++ app/src/main/java/com/lau/studyassistance/MainActivity.java
@@ -2,10 +2,13 @@ package com.lau.studyassistance;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+
+    int code_index, name_index;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,11 +18,17 @@ public class MainActivity extends AppCompatActivity {
         try{
             SQLiteDatabase sql = this.openOrCreateDatabase("final_exam_db", MODE_PRIVATE, null);
             sql.execSQL("CREATE Table IF NOT EXISTS exams (code VARCHAR, name VARCHAR)");
-            sql.execSQL("INSERT INTO exams (code, name) VALUES ('CSC498G', 'Mobile Computing')");
-            sql.execSQL("INSERT INTO exams (code, name) VALUES ('HST242H', 'History - Europe & M.E.')");
-            sql.execSQL("INSERT INTO exams (code, name) VALUES ('MEE548', 'Introduction to Biomechatronics')");
-            sql.execSQL("INSERT INTO exams (code, name) VALUES ('GNE340', 'Engineering Entrepreneurship')");
+//            sql.execSQL("INSERT INTO exams (code, name) VALUES ('CSC498G', 'Mobile Computing')");
+//            sql.execSQL("INSERT INTO exams (code, name) VALUES ('HST242H', 'History - Europe & M.E.')");
+//            sql.execSQL("INSERT INTO exams (code, name) VALUES ('MEE548', 'Introduction to Biomechatronics')");
+//            sql.execSQL("INSERT INTO exams (code, name) VALUES ('GNE340', 'Engineering Entrepreneurship')");
 
+            Cursor cursor = sql.rawQuery("SELECT * FROM exams", null);
+            code_index = cursor.getColumnIndex("code");
+            name_index = cursor.getColumnIndex("name");
+            cursor.moveToFirst();
+
+            
 
         }catch(Exception e){
             e.printStackTrace();
