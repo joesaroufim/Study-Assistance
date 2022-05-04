4322ca82d2cc41959dea25db03f3a67b6ffa8cf6
Joe Saroufim <joesaroufim.os@gmail.com>
2022-05-04 13:56:57 +0300

    Inserted values into the table

--- app/src/main/java/com/lau/studyassistance/MainActivity.java
+++ app/src/main/java/com/lau/studyassistance/MainActivity.java
@@ -13,8 +13,13 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         try{
-            SQLiteDatabase sql = this.openOrCreateDatabase("final_examdb", MODE_PRIVATE, null);
-            sql.execSQL("CREATE Table IF NOT EXISTS exams (name VARCHAR, code VARCHAR)");
+            SQLiteDatabase sql = this.openOrCreateDatabase("final_exam_db", MODE_PRIVATE, null);
+            sql.execSQL("CREATE Table IF NOT EXISTS exams (code VARCHAR, name VARCHAR)");
+            sql.execSQL("INSERT INTO exams (code, name) VALUES ('CSC498G', 'Mobile Computing')");
+            sql.execSQL("INSERT INTO exams (code, name) VALUES ('HST242H', 'History - Europe & M.E.')");
+            sql.execSQL("INSERT INTO exams (code, name) VALUES ('MEE548', 'Introduction to Biomechatronics')");
+            sql.execSQL("INSERT INTO exams (code, name) VALUES ('GNE340', 'Engineering Entrepreneurship')");
+
 
         }catch(Exception e){
             e.printStackTrace();
