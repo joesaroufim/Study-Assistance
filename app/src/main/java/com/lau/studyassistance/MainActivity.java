afdd7b9b4805cee76c83b2f5c8e5985bf7a9d20e
Joe Saroufim <joesaroufim.os@gmail.com>
2022-05-04 13:50:39 +0300

    Created a table in the local database

--- app/src/main/java/com/lau/studyassistance/MainActivity.java
+++ app/src/main/java/com/lau/studyassistance/MainActivity.java
@@ -2,6 +2,7 @@ package com.lau.studyassistance;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,5 +11,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        try{
+            SQLiteDatabase sql = this.openOrCreateDatabase("final_examdb", MODE_PRIVATE, null);
+            sql.execSQL("CREATE Table IF NOT EXISTS exams (name VARCHAR, code VARCHAR)");
+
+        }catch(Exception e){
+            e.printStackTrace();
+        }
     }
 }
